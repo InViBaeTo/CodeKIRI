38b4a974b31f4eb0dab6929ce36b97c4f8591289
smhrd5 <smhrd5@192.168.219.104>
2024-10-21 12:26:06 +0900

    정제우/ 백업

--- DFD/src/main/java/com/DFD/dao/userDAO.java
+++ DFD/src/main/java/com/DFD/dao/userDAO.java
@@ -7,12 +7,11 @@ import com.DFD.database.FactoryManager;
 import com.DFD.entity.DFD_USER;
 
 public class userDAO {
+	
 	private SqlSessionFactory factory = FactoryManager.getSqlSessionFactory();
-
+	
 	public DFD_USER login(DFD_USER member) {
 		SqlSession session = factory.openSession(true);
-		// selectOne  : 결과가 한개  ResultType 에 명시한 자료형
-		// selectlist : 결과가 여러개 List<ResultType>
 		DFD_USER result = session.selectOne("login", member);
 		session.close();
 		return result;
